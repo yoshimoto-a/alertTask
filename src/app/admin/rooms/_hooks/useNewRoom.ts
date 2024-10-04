4509bdf2b7d1efd2558d1cd6f5b28335894fa7c0
rs-akane <rs.akane.y@gmail.com>
2024-10-04 13:14:39 +0900

    ルーム登録フック

---
+++ src/app/admin/rooms/_hooks/useNewRoom.ts
@@ -0,0 +1,43 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { useApi } from "@/app/_hooks/useApi";
+import { PostRequest } from "@/app/_types/admin/room/PostRequest";
+import { PostResponse } from "@/app/_types/admin/room/PostResponse";
+// import toast from "react-hot-toast";
+
+interface NewRoom {
+  token: string;
+}
+
+export const useNewRoom = () => {
+  const { post } = useApi();
+  const schema = z.object({
+    token: z.string().min(1, { message: "APIトークンは必須です" }),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<NewRoom>({
+    resolver: zodResolver(schema),
+    mode: "onSubmit",
+  });
+
+  const onSubmit = async (formdata: NewRoom) => {
+    try {
+      await post<PostRequest, PostResponse>("/api/admin/room", {
+        lineToken: formdata.token,
+      });
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  return {
+    register,
+    handleSubmit: handleSubmit(onSubmit),
+    errors,
+    isSubmitting,
+  };
+};
