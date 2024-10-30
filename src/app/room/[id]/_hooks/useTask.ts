7f51737c7f69113e916357a15e255048e3d0831d
rs-akane <rs.akane.y@gmail.com>
2024-10-30 14:00:21 +0900

    path修正

--- src/app/room/[id]/_hooks/useTask.ts
+++ src/app/room/[id]/_hooks/useTask.ts
@@ -1,7 +1,7 @@
 import { useFetch } from "@/app/_hooks/useFetch";
 import { DetailResponse } from "@/app/_types/task/DetailResponse";
 export const useTask = (taskId: number) => {
-  const { data, error } = useFetch<DetailResponse>(`tasks/${taskId}`);
+  const { data, error } = useFetch<DetailResponse>(`api/tasks/${taskId}`);
 
   return { data, error };
 };
